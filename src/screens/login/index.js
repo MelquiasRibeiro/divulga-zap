ff646bd57f741d8a44bbc7f20f5e8615512e6017
Melquias Ribeiro <melquias10.mr@gmail.com>
2020-07-02 18:10:33 -0300

    feat: initial structure and navigation

---
+++ src/screens/login/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const  Login = () => {
+  return (
+    <View>
+        <Text>
+            Login
+        </Text>
+    </View>
+
+  );
+}
+
+export default Login;
