e848e5a976af67dd42531bcc1d911410fa57c81d
Melquias Ribeiro <melquias10.mr@gmail.com>
2020-07-13 19:12:33 -0300

    feat: splash and logo

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import Routes from './src/routes';
-import { StatusBar } from 'expo-status-bar';
-
 
 export default function App() {
-  return (
-   <>
-      <Routes/>
-   </>
-  );
+    return (
+        <>
+            <Routes />
+        </>
+    );
 }
-
-
